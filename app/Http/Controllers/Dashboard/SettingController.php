4b91e2427e381bd7fe4d7aa75e55cb9ba5dad03f
MahmoudDhair <mdhair2000@students.iugaza.edu.ps>
2020-12-12 12:09:42 +0200

    finshed Shipping

--- app/Http/Controllers/Dashboard/SettingController.php
+++ app/Http/Controllers/Dashboard/SettingController.php
@@ -3,25 +3,43 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ShippingsRequest;
 use App\Models\Setting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SettingController extends Controller
 {
-    public function editShippingMethod($type){
-        if($type === 'free')
-            $shippingMethod = Setting::where('key','free_shipping_label')->first();
+    public function editShippingMethod($type)
+    {
+
+        if ($type === 'free')
+            $shippingMethod = Setting::where('key', 'free_shipping_label')->first();
         elseif ($type === 'inner')
-            $shippingMethod = Setting::where('key','local_label')->first();
+            $shippingMethod = Setting::where('key', 'local_label')->first();
         elseif ($type === 'outer')
-            $shippingMethod = Setting::where('key','outer_label')->first();
+            $shippingMethod = Setting::where('key', 'outer_label')->first();
         else
-            $shippingMethod = Setting::where('key','free_shipping_label')->first();
+            $shippingMethod = Setting::where('key', 'free_shipping_label')->first();
 
-        return view('dashboard.settings.shipping.edit',compact('shippingMethod'));
+        return view('dashboard.settings.shipping.edit', compact('shippingMethod'));
     }
 
-    public function updateShippingMethod(Request $request , $id){
+    public function updateShippingMethod(ShippingsRequest $request, $id)
+    {
+        try {
+            DB::beginTransaction();
+            $shipping_method = Setting::find($id);
+            $shipping_method->update(['plain_value' => $request->plain_value]);
+            $shipping_method->value = $request->value;
+            $shipping_method->save();
+            DB::commit();
+            return redirect()->back()->with(['success' => 'تم التعديل بنجاح']);
+        } catch (\Exception $ex) {
+            DB::rollBack();
+            return redirect()->back()->with(['error' => 'حاول مرة اخرى']);
+        }
 
     }
+
 }
